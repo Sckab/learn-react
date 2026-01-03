3667decccb8465061fd15945815783932b91d558
Sckab <giulianodeamicis2009@gmail.com>
2026-01-03 02:20:25 +0100

    reduced the header title size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ export default function Header({
   return (
     <header className='flex justify-center bg-shadow-grey-800 py-3'>
       <div className='mx-3 flex w-full items-center justify-between md:mx-0 md:w-2/3 '>
-        <h1 className='text-4xl font-bold text-powder-blue-300 uppercase'>
+        <h1 className='text-2xl font-bold text-powder-blue-300 uppercase sm:text-4xl'>
           {title}
         </h1>
         <Link link='https://github.com/Sckab/learn-react'>Github repo</Link>
